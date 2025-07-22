98b892d3f88a453bc9a4c13cac24c90b07bf5bda
kenster1965 <72036697+kenster1965@users.noreply.github.com>
2025-07-22 14:27:11 -0600

    Prep for 1.0.0

--- const.py
+++ const.py
@@ -6,7 +6,7 @@ import os
 NAME = "Sifely Cloud"
 DOMAIN = "sifely_cloud"
 ENTITY_PREFIX = "sifely" # Prefix for entity names
-VERSION = "1.5.0"
+VERSION = "1.0.0"
 ISSUE_URL = "https://github.com/kenster1965/sifely_cloud/issues"
 
 CONF_EMAIL = "User_Email"#
